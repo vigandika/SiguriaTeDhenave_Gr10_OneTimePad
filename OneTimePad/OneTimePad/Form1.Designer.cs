4d296ef83ac89fdeed485dca6c268c61e82a2942
Vegim-Bytyqi <44346620+Vegim-Bytyqi@users.noreply.github.com>
2019-05-01 15:11:28 +0200

    labela

--- OneTimePad/OneTimePad/Form1.Designer.cs
+++ OneTimePad/OneTimePad/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.txtCiphertexti = new System.Windows.Forms.TextBox();
             this.btnEnkripto = new System.Windows.Forms.Button();
             this.btnDekripto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtPlaintexti
@@ -99,11 +103,51 @@
             this.btnDekripto.Text = "Dekripto";
             this.btnDekripto.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Plain teksti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Celesi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Teksti i enkriptuar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Teksti i dekriptuar";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDekripto);
             this.Controls.Add(this.btnEnkripto);
             this.Controls.Add(this.txtCiphertexti);
@@ -125,6 +169,10 @@
         private System.Windows.Forms.TextBox txtCiphertexti;
         private System.Windows.Forms.Button btnEnkripto;
         private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
